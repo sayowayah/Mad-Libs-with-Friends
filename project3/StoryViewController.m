f8770a3de982ffc87f3e0127506ae86e8d8d88bd
sayo's MBA <sayowayah@gmail.com>
2012-05-03 03:05:47 -0700

    finished completed story screen

--- project3/StoryViewController.m
+++ project3/StoryViewController.m
@@ -20,11 +20,11 @@
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
 }
 
 - (IBAction)exit:(id)sender{
@@ -33,33 +33,49 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    self.title = @"Completed Story";
-  // TODO: hide back button
-    int numberOfScreens = ([self.completedStoryText length] / 600) + 0.5;
-    self.scroll.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height*numberOfScreens);  
-  UILabel *story = [[UILabel alloc] initWithFrame:CGRectMake(50, 10, 300, self.view.frame.size.height*numberOfScreens)];
+  [super viewDidLoad];
+  self.title = @"Completed Story";
   
-  // TODO: fix scroll
-  // TODO: add button
-    story.text = self.completedStoryText;
+  self.navigationItem.hidesBackButton = YES;
+  
+  UILabel *story = [[UILabel alloc] initWithFrame:CGRectMake(20, 50, 280, 10)];
+  story.lineBreakMode = UILineBreakModeWordWrap;
+  story.numberOfLines = 0;
+  
+  //Calculate the expected size based on the font and linebreak mode of story label
+  CGSize maximumLabelSize = CGSizeMake(280,9999);
+  CGSize expectedLabelSize = [self.completedStoryText sizeWithFont:story.font 
+                                                 constrainedToSize:maximumLabelSize 
+                                                     lineBreakMode:story.lineBreakMode]; 
+  //adjust the label to the new height.
+  CGRect newFrame = story.frame;
+  newFrame.size.height = expectedLabelSize.height;
+  story.frame = newFrame;
+  
+  NSString *text = [[NSString alloc] initWithString:self.completedStoryText];
+  [story setText:text];
+  
   [self.scroll addSubview:story];
   
+  self.scroll.contentSize = CGSizeMake(self.view.frame.size.width, newFrame.size.height + 200);
   
-  // about 30 characters per line, about 20 lines per screen = 600 characters
-
+  UIButton *exit = [[UIButton alloc] initWithFrame:CGRectMake(100, 80 + newFrame.size.height, 120, 50)];
+  [exit setBackgroundColor:[UIColor blueColor]];
+  [exit setTitle:@"Exit" forState:UIControlStateNormal];
+  [exit addTarget:self action:@selector(exit:) forControlEvents:UIControlEventTouchUpInside];
+  
+  [self.scroll addSubview:exit];
+  
 }
 
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+  [super viewDidUnload];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+  return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 @end
